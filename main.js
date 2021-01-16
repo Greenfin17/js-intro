c9f98a419562e39b57ef0d93f62ca55a331a1641
Greenfin17 <jmaple@yahoo.com>
2021-01-16 11:58:17 -0600

    added function declarations and calls

--- main.js
+++ main.js
@@ -6,3 +6,35 @@ function showDate() {
 }
 
 console.log("Connected");
+sayHello();
+
+function sayHello() {
+  console.log("Hello");
+}
+
+
+/*
+const sayHello = function() {
+  console.log("Hello");
+}
+
+  const sayHello = () => {
+  console.log("Hello");
+}
+*/
+/********* FUNCTIONS WITH ARGUMENTS */
+// function logsHello(name){
+//   console.log(`Hello ${name}`);
+// }
+
+// logsHello('John');
+
+const sum = 55;
+
+const addTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log("Console log inside the function: " + sum);
+  return sum;
+}
+console.log(addTwoNumbers(13,7));
+console.log("Console log of global scope variable " + sum); 
