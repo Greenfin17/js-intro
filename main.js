11556959b1ab2bf0dc90747d5a8237b03f6748f9
Greenfin17 <jmaple@yahoo.com>
2021-01-16 11:08:11 -0600

    added variablesl, data types and math to js file

--- main.js
+++ main.js
@@ -6,3 +6,41 @@ function showDate() {
 }
 
 console.log("Connected");
+// VARIABLES
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+const favoriteFood = 'Fish Tacos';
+// ********** END VARIABLES
+
+// ********** START DATA TYPES***********
+
+const dog = 'bark';
+const answer = 33;
+const negative = -14.23;
+const iLoveJavascript = true;
+
+console.log(typeof dog, typeof answer, typeof negative);
+console.log(typeof iLoveJavascript);
+
+
+// **********  END DATA TYPES***********
+
+
+// **********  START MATH ***********
+1 + 1
+console.log(Math.pow(2,2)); //4
+console.log(Math.pow(3,2)); //9
+console.log(Math.pow(3,3)); //27
+console.log(Math.round(6.5)); //
+console.log(Math.round(6.45)); //6
+console.log(Math.floor(6.99)); //6
+console.log(Math.ceil(6.99)); //7
+console.log(Math.ceil(6.0001)); //7
+
+const cat = 'kitty';
+console.log(cat.length);
+
+
+// **********  END MATH ***********
+
+
